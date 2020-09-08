f7779bbc55493026a78c88799ed9ad9605712417
Samuel Wessén <SamuelWessen@hotmail.com>
2020-09-08 16:54:04 +0100

    Add project files.

---
+++ Writelinetest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Writelinetest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
